ad20108d45fd2e99681c73f0d1ebfa7eda4b7f96
Styl Bandeira <stylbandeira@hotmail.com>
2020-10-24 20:39:29 -0300

    iniciamos nosso projeto

---
+++ class/Sql.php
@@ -0,0 +1,33 @@
+<?php 
+class Sql extends PDO{
+	private $conn;
+
+	public function __construct(){
+		$this->conn = new PDO("mysql:host=localhost; dbname=dbphp7", "root", "");
+	}
+
+	private function setParams($statement, $parameters = array()){
+		foreach ($parameters as $key => $value) {
+			$this->setParam($key, $value);
+		}
+	}
+
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
+	}
+
+	public function query($rawQuery, $params = array()){
+		$stmt = $this->conn->prepare($rawQuery);
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+		return $stmt;
+	}
+
+	public function select($rawQuery):array{
+		$stmt = $this->query($rawQuery);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+}
+ ?>
