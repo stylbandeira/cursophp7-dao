4814259b6ef3bd2881ae9cd8e6a8477eb42733e1
Styl Bandeira <stylbandeira@hotmail.com>
2020-10-26 16:49:12 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -37,8 +37,10 @@ class Usuario{
 		$this->dtcadastro = $dtcadastro;
 	}
 
+	//pega um usuário com aquele $id
 	public function loadById($id){
 		$sql = new Sql();
+		//usei a variável $query para passar somente um argumento no select, pois ele estava acusando dois argumentos
 		$query = "SELECT * FROM tb_usuarios WHERE idusuario = ".$id;
 
 		$results = $sql->select($query);
@@ -48,6 +50,39 @@ class Usuario{
 			$this->setDeslogin($row['deslogin']);
 			$this->setDessenha($row['dessenha']);
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}
+	}
+
+	//traz uma lista de todos os usuários
+	public static function getList(){
+		$sql = new Sql();
+		$query = "SELECT * FROM tb_usuarios ORDER BY deslogin;";
+
+		return $sql->select($query);
+
+	}
+
+	public static function searchUsuario($login){
+		$sql = new Sql();
+		$query = "SELECT * FROM tb_usuarios WHERE deslogin LIKE '%".$login."%';";
+
+		return $sql->select($query);
+	}
+
+	public function login($login, $password){
+		$sql = new Sql();
+		$query = "SELECT * FROM tb_usuarios WHERE deslogin = '".$login."' AND dessenha = '". $password."'";
+
+		$results = $sql->select($query);
+		if (count($results) > 0) {
+			$row = $results[0];
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos.");
 		}
 	}
 
--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php 
 require_once("config.php");
+/*
+//CARREGA UM USUÁRIO:
+
 $root = new Usuario();
-
 $root->loadById(6);
-
 echo $root;
+*/
+
+//CARREGA UMA LISTA DE USUÁRIOS:
+//O método pode ser chamado usando :: por ser Static (e também por não depender de nenhum parâmetro)
+/*$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+//CARREGA LISTA DE USUÁRIOS BUSCANDO PELO LOGIN:
+/*$search = Usuario::searchUsuario("bre");
+echo json_encode($search);*/
+
+//CARREGA UM USUÁRIO COM LOGIN E SENHA
+$usuario = new Usuario();
+$usuario->login("brenoCosta", "amaizaia"); 
+
+echo $usuario;
  ?>
